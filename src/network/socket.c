f85cd1b8cd141af7daa278c67eb38fb0834173f2
Kobayashi82 <kobayashi82@outlook.com>
2025-07-28 15:13:56 +0200

    quitar modo promiscuo

--- src/network/socket.c
+++ src/network/socket.c
@@ -6,7 +6,7 @@
 /*   By: vzurera- <vzurera-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 20:13:37 by vzurera-          #+#    #+#             */
-/*   Updated: 2025/07/28 15:10:20 by vzurera-         ###   ########.fr       */
+/*   Updated: 2025/07/28 15:13:45 by vzurera-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@
 		}
 
 		// Promiscuous mode
-		struct packet_mreq mreq;
-		ft_memset(&mreq, 0, sizeof(mreq));
-		mreq.mr_ifindex = g_malcolm.if_index;
-		mreq.mr_type = PACKET_MR_PROMISC;
-		if (!mreq.mr_ifindex || setsockopt(g_malcolm.sockfd, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
-			fprintf(stderr, "Failed to set promiscuous mode on %s\n", g_malcolm.if_name);
-		} else {
-			fprintf(stderr, "Promiscuous mode on %s\n", g_malcolm.if_name);
-		}
+		// struct packet_mreq mreq;
+		// ft_memset(&mreq, 0, sizeof(mreq));
+		// mreq.mr_ifindex = g_malcolm.if_index;
+		// mreq.mr_type = PACKET_MR_PROMISC;
+		// if (!mreq.mr_ifindex || setsockopt(g_malcolm.sockfd, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+		// 	fprintf(stderr, "Failed to set promiscuous mode on %s\n", g_malcolm.if_name);
+		// } else {
+		// 	fprintf(stderr, "Promiscuous mode on %s\n", g_malcolm.if_name);
+		// }
 
 		return (0);
 	}
